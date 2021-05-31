23e08c3a6fc47aef900b1a986532b2dd66d05bfd
Semisol <git@smileycreations15.com>
2021-05-31 21:28:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const validateKey = (() => {
     }
   }
 })();
-
+let majors = ["tdm", "teamdeathmatch", "ragepit", "raffle", "beast", "squads", "blockhead", "robbery", "spire", "pizza", "delivery"]
 app.use('/', async (req, res) => {
   // remove expired events
   while(events.length && events[0].timestamp < Date.now()) events.shift();
@@ -128,5 +128,12 @@ app.use('/', async (req, res) => {
   // without a key limit to only the next 5 events
   res.send(events.slice(0, 5));
 });
-
+app.use('/1major3minor', async (req, res) => { // UNTESTED
+  // remove expired events
+  while(events.length && events[0].timestamp < Date.now()) events.shift();
+  let minor = events.filter(x=>!majors.includes(x.event.replace(/ +/g, "").toLowerCase())).slice(0, 3)
+  while (minors.length < 3) {minors.push({event: "None", timestamp: 0})}
+  let major = events.find(x=>majors.includes(x.event.replace(/ +/g, "").toLowerCase())) || {event: "None", timestamp: 0}
+  res.send({major, minor})
+});
 app.listen(5002, () => console.log('API running on port 5002'));
